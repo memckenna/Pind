9e4f28ff08cfe77a8bd61e3ca29ff1d7ea738f54
Megan McKenna <meganemckenna1@gmail.com>
2022-02-23 18:00:14 -0600

    updated validation errors for boards

--- app/forms/board_form.py
+++ app/forms/board_form.py
@@ -4,8 +4,9 @@ from wtforms.validators import DataRequired, ValidationError, Length
 from app.models import User
 from wtforms.fields import FileField
 
+
 class CreateBoardForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(message='Please provide a title')])
+    title = StringField('Title', validators=[DataRequired(), Length(max=70, message='Title must be less than 70 characters long.')])
     # description = TextAreaField('Description')
 
 class EditBoardForm(FlaskForm):
--- app/forms/pin_form.py
+++ app/forms/pin_form.py
@@ -7,7 +7,8 @@ from wtforms.fields import FileField
 
 
 class CreatePinForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(message='Please provide a title')])
+    title = StringField('Title', validators=[DataRequired(message='Please provide a title'),
+                        Length(min=10, message='Title must be less than 70 characters.')])
     description = TextAreaField('Description')
     source_link = StringField('Source Link')
     photo_url = StringField('Photo URL',validators=[DataRequired()])
