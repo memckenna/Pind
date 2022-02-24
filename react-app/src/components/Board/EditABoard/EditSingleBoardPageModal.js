f741575dd9862a61be62072e11664b30a37cc976
Megan McKenna <meganemckenna1@gmail.com>
2022-02-24 12:01:33 -0600

    completed readme

---
+++ react-app/src/components/Board/EditABoard/EditSingleBoardPageModal.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { EditBoardModal } from "../../../context/Modal";
+import EditABoardForm from "./EditABoardForm";
+import './EditBoard.css';
+
+
+const EditSingleBoardPageModal = () => {
+    const [showModal, setShowModal] = useState(false);
+    const sessionUser = useSelector(state => state.session.user)
+
+    const onCloseModal = () => {
+        setShowModal(false)
+        // if (onCloseMenuModal) {
+        //     onCloseMenuModal()
+
+        // }
+    }
+
+    return (
+        <>
+            {/* <div className="edit-board-modal"> */}
+                {/* {sessionUser.id === board.user_id && (
+                    <button className="edit-pen-btn">
+                        <i
+                            className="fas fa-ellipsis-h"
+                            onClick={() => setShowModal(true)}
+                        ></i>
+                    </button>
+                 )}
+                {showModal && (
+                    <EditBoardModal onClose={() => setShowModal(false)}>
+                        <EditABoardForm id={id} onClose={onCloseModal} />
+                    </EditBoardModal>
+                )} */}
+            {/* </div> */}
+        </>
+    )
+}
+
+export default EditSingleBoardPageModal;
